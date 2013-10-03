a89b497da8404524c2075e273fc49ec129b0c15d
Devon Tivona <tivona@me.com>
2013-10-03 14:50:49 -0600

    Add filtering of movies by rating and save state in user session

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,7 +5,32 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.order(params[:sort])
+    @all_ratings = Movie.all_ratings
+    redirect = false
+
+    if params.has_key? :ratings
+      @current_ratings = params[:ratings].keys
+    else
+      redirect = true
+      @current_ratings = session.has_key?(:ratings) ? session[:ratings] : @all_ratings
+    end
+
+    if params.has_key? :sort
+      @sort_order = params[:sort]
+    elsif session.has_key? :sort
+      redirect, @sort_order = true, session[:sort]
+    else
+      @sort_order = nil
+    end
+
+    if redirect
+      flash.keep
+      ratings = @current_ratings.inject({}) { |h, r| h[r] = 1; h }
+      redirect_to movies_path(ratings: ratings, sort: @sort_order)
+    else
+      session[:ratings], session[:sort] = @current_ratings, @sort_order
+      @movies = Movie.where("rating IN (?)", @current_ratings).order(@sort_order)
+    end
   end
 
   def new
