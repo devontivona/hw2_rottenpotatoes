f720719c98a12c456244d78b798a74dfdc0cd48e
Devon Tivona <tivona@me.com>
2013-10-03 13:59:26 -0600

    Add ordering to movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,17 +1,14 @@
 class MoviesController < ApplicationController
 
   def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
+    @movie = Movie.find(params[:id])
   end
 
   def index
-    @movies = Movie.all
+    @movies = Movie.order(params[:sort])
   end
 
   def new
-    # default: render 'new' template
   end
 
   def create
